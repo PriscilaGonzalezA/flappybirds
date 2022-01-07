8448546ec46ee3aa7915976aa6d8bbe8ee5cea3f
priscilagonzalezadrian <priscila.gonzalez@thallus.cl>
2022-01-07 11:27:55 -0300

    se ven los tubos

--- App.js
+++ App.js
@@ -1,9 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
+import Bird from "./components/Bird";
+import React, {useEffect, useState} from "react";
+import Obstacles from "./components/Obstacles";
 
 export default function App() {
+  const screenWidth = Dimensions.get('screen').width
+  const screenHeight = Dimensions.get('screen').height
+  const birdLeft = screenWidth / 2
+  const [birdBottom, setBirdBottom] = useState(screenHeight/2)
+  const [obstaclesLeft, setObstaclesLeft] = useState(screenWidth)
+  const gravity = 3
+  const obstacleWidth = 60
+  const obstacleHeight = 300
+  const gap = 200
+  let gameTimerId
+  let obstaclesLeftTimerId
+
+  //start bird falling
+  useEffect(() => {
+    if(birdBottom > 0){
+      gameTimerId = setInterval(()=>{
+        setBirdBottom(birdBottom => birdBottom - gravity)
+      }, 30)
+
+      return () => {
+        clearInterval(gameTimerId)
+      }
+    }
+  },[birdBottom])
+
+  //start first obstacles
+  useEffect(()=> {
+    if(obstaclesLeft > -obstacleWidth){
+      obstaclesLeftTimerId = setInterval(()=> {
+        setObstaclesLeft(obstaclesLeft => obstaclesLeft - 5)
+      },30)
+
+      return () => {
+        clearInterval(obstaclesLeftTimerId)
+      }
+    }else{
+      setObstaclesLeft(screenWidth)
+    }
+
+
+  },[obstaclesLeft])
+
+
   return (
     <View style={styles.container}>
-      <Text>Hola Priscila!</Text>
+      <Bird
+          birdBottom={birdBottom}
+          birdLeft={birdLeft}
+      />
+      <Obstacles
+          obstacleWidth={obstacleWidth}
+          obstacleHeight={obstacleHeight}
+          obstacleLeft={obstaclesLeft}
+          gap={gap}
+      />
     </View>
   );
 }
